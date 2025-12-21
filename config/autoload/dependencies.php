2cb2e42e6ab0b9020ca98b74b6ad42baab3541f2
Carlos Ferreira <carlos.ferreira@onfly.com.br>
2025-12-21 20:16:41 -0300

    feat(dependencies): add transaction services and repositories to the dependency injection

--- config/autoload/dependencies.php
+++ config/autoload/dependencies.php
@@ -15,6 +15,12 @@ declare(strict_types=1);
 
 use App\Modules\Auth\Domain\Services\TokenServiceInterface;
 use App\Modules\Auth\Infra\Services\JwtService;
+use App\Modules\Transaction\Domain\Repositories\TransactionRepositoryInterface;
+use App\Modules\Transaction\Domain\Services\AuthorizationServiceInterface;
+use App\Modules\Transaction\Domain\Services\NotificationServiceInterface;
+use App\Modules\Transaction\Infra\Repositories\TransactionRepository;
+use App\Modules\Transaction\Infra\Services\DeviToolsAuthorizationService;
+use App\Modules\Transaction\Infra\Services\RabbitMQNotificationService;
 use App\Modules\User\Domain\Repositories\UserDocumentRepositoryInterface;
 use App\Modules\User\Domain\Repositories\UserRepositoryInterface;
 use App\Modules\User\Infra\Repositories\UserDocumentRepository;
@@ -23,8 +29,18 @@ use App\Modules\Wallet\Domain\Repositories\WalletRepositoryInterface;
 use App\Modules\Wallet\Infra\Repositories\WalletRepository;
 
 return [
+    // User
     UserRepositoryInterface::class => UserRepository::class,
     UserDocumentRepositoryInterface::class => UserDocumentRepository::class,
+
+    // Wallet
     WalletRepositoryInterface::class => WalletRepository::class,
+
+    // Auth
     TokenServiceInterface::class => JwtService::class,
+
+    // Transaction
+    TransactionRepositoryInterface::class => TransactionRepository::class,
+    AuthorizationServiceInterface::class => DeviToolsAuthorizationService::class,
+    NotificationServiceInterface::class => RabbitMQNotificationService::class,
 ];
