1198fea8a2306ccf27b2c4676c43dea817bbecd5
Carlos Ferreira <carlos.ferreira@onfly.com.br>
2025-12-21 20:11:40 -0300

    refactor(User): remove redundant setId method from User class

--- app/Modules/User/Domain/Entity/User.php
+++ app/Modules/User/Domain/Entity/User.php
@@ -43,11 +43,6 @@ class User
         $this->updatedAt = $updatedAt ?? new DateTimeImmutable();
     }
 
-    public function setId(int $id): void
-    {
-        $this->id = $id;
-    }
-
     public function getId(): int
     {
         return $this->id;
