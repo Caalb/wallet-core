050fabf20becb98f2bfad682c0f1ccd70f5d6b93
Carlos Ferreira <carlos.ferreira@onfly.com.br>
2025-12-22 19:29:54 -0300

    fix(bootstrap): enable coroutine with SWOOLE_HOOK_FLAGS and set testing environment configurations

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,12 +1,13 @@
 <?php
 
 declare(strict_types=1);
+
 use Hyperf\Contract\ApplicationInterface;
 use Hyperf\Di\ClassLoader;
 use Hyperf\Engine\DefaultOption;
 use Swoole\Runtime;
 
-/**
+/*
  * This file is part of Hyperf.
  *
  * @see     https://www.hyperf.io
@@ -17,13 +18,12 @@ use Swoole\Runtime;
  *
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+
 ini_set('display_errors', 'on');
 ini_set('display_startup_errors', 'on');
 
 error_reporting(E_ALL);
 date_default_timezone_set('Asia/Shanghai');
-
-Runtime::enableCoroutine(true);
 
 !defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
 
@@ -31,8 +31,15 @@ require BASE_PATH . '/vendor/autoload.php';
 
 !defined('SWOOLE_HOOK_FLAGS') && define('SWOOLE_HOOK_FLAGS', DefaultOption::hookFlags());
 
+Runtime::enableCoroutine(SWOOLE_HOOK_FLAGS);
+
 ClassLoader::init();
 
 $container = require BASE_PATH . '/config/container.php';
+
+$config = $container->get(Hyperf\Contract\ConfigInterface::class);
+$config->set('app_env', 'testing');
+
+$config->set('databases.default.database', 'wallet-core-test');
 
 $container->get(ApplicationInterface::class);
