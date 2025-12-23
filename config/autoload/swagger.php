a808df98560604ec14895ddda94e6470073511d8
Carlos Ferreira <carlos.ferreira@onfly.com.br>
2025-12-23 00:32:27 -0300

    feat: add Swagger configuration for API documentation

---
+++ config/autoload/swagger.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Hyperf.
+ *
+ * @see     https://www.hyperf.io
+ *
+ * @document https://hyperf.wiki
+ *
+ * @contact  group@hyperf.io
+ *
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+return [
+    'enable' => true,
+    'port' => 9502,
+    'json_dir' => BASE_PATH . '/docs',
+    'html' => <<<'HTML'
+        <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta
+              name="description"
+              content="SwaggerUI"
+            />
+            <title>SwaggerUI</title>
+            <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css" />
+          </head>
+          <body>
+          <div id="swagger-ui"></div>
+          <script src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-bundle.js" crossorigin></script>
+          <script src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-standalone-preset.js" crossorigin></script>
+          <script>
+            window.onload = () => {
+              window.ui = SwaggerUIBundle({
+                url: GetQueryString("search"),
+                dom_id: '#swagger-ui',
+                presets: [
+                  SwaggerUIBundle.presets.apis,
+                  SwaggerUIStandalonePreset
+                ],
+                layout: "StandaloneLayout",
+              });
+            };
+            function GetQueryString(name) {
+              var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+              var r = window.location.search.substr(1).match(reg);
+              var context = "";
+              if (r != null)
+                context = decodeURIComponent(r[2]);
+              reg = null;
+              r = null;
+              return context == null || context == "" || context == "undefined" ? "/http.json" : context;
+            }
+          </script>
+          </body>
+        </html>
+        HTML,
+    'url' => '/swagger',
+    'auto_generate' => false,
+    'scan' => [
+        'paths' => [],
+    ],
+];
